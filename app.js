c5d1c24699361ab506289dd614b3ee4e11958036
Srihari Nanniyur <snanniyur@localhost.localdomain>
2019-06-26 19:04:40 -0700

    update

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var nunjucks = require('nunjucks');
+var path = require('path');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+var PATH_TO_TEMPLATES = './templates/' ;
+nunjucks.configure(PATH_TO_TEMPLATES, {
+    autoescape: true,
+    express: app
+});
+
+app.get('/', function(req, res) {
+    return res.render('index.html');
+});
+
+app.listen(8080);
